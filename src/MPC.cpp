9e5a2400c37a10a69005c206123ff5fe26424271
Diogo Pontes <dpontes11@gmail.com>
2018-01-12 15:50:14 +0100

    Work In Progress

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -5,9 +5,9 @@
 
 using CppAD::AD;
 
-// TODO: Set the timestep length and duration
-size_t N = 0;
-double dt = 0;
+// MPC solver number of timesteps and duration of each step
+constexpr size_t kNSteps = 8;     // number of state timesteps
+constexpr double kDeltaT = 0.15;  // time per step (sec)
 
 // This value assumes the model presented in the classroom is used.
 //
@@ -19,20 +19,74 @@ double dt = 0;
 // presented in the classroom matched the previous radius.
 //
 // This is the length from front to CoG that has a similar radius.
-const double Lf = 2.67;
+const double kLf = 2.67;  // m
 
+// Target velocity for cost function
+// - Boosted to 115mph to have some margin for cost function to achieve
+//   actual 100mph peak speed on lake track
+constexpr double kRefVMax = 115;          // mph
+constexpr double kRefVTurnFactor = 0.2;   // tuned factor to reduce speed in turns
+
+// Set index points for varaible starting locations in the combined vector
+const size_t x_start     = 0;
+const size_t y_start     = x_start     + kNSteps;
+const size_t psi_start   = y_start     + kNSteps;
+const size_t v_start     = psi_start   + kNSteps;
+const size_t cte_start   = v_start     + kNSteps;
+const size_t epsi_start  = cte_start   + kNSteps;
+const size_t delta_start = epsi_start  + kNSteps;
+const size_t a_start     = delta_start + (kNSteps - 1); // N-1 delta actuators values
+
+/*
+  The FG_eval object sets up the MPC's cost function and constraints to be
+  used as an input to the IPOPT optimizer for solving the time steps by the
+  CppAD::ipopt::solve() method.
+*/
 class FG_eval {
  public:
-  // Fitted polynomial coefficients
+  // Fitted waypoint polynomial coefficients
   Eigen::VectorXd coeffs;
+
+  // Constructor
   FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
 
+  // ADvector typedef for convenience
   typedef CPPAD_TESTVECTOR(AD<double>) ADvector;
+
+  /*
+    The operator() method uses 'vars' CppAD vector of state variables
+    to calculate the 'fg' vector that contains the cost at index 0,
+    and the state constraints for each timestep at index 1~(kNSteps+1)
+    based on the motion model differential equations with the form
+    x1 = f(x0) -> 0 = x1 - f(x0).
+   */
   void operator()(ADvector& fg, const ADvector& vars) {
-    // TODO: implement MPC
-    // `fg` a vector of the cost constraints, `vars` is a vector of variable values (state & actuators)
-    // NOTE: You'll probably go back and forth between this function and
-    // the Solver function below.
+    /*
+      Define cost function to be minimized
+
+      Estimate endpoint of planned path poly coefficients after kNSteps
+      of kDeltaT (x = v*t, y = f(x)). Calculate the angle from the car's
+      current heading (0 degrees) to the endpoint of the planned path to use
+      as a cost for difference from reference angle to be able to pull the
+      path straighter across and cut corners in sharp turns to achieve higher
+      speed and a better driving line
+    */
+    const CppAD::AD<double> x_end = vars[v_start] * (kNSteps * kDeltaT);
+    const CppAD::AD<double> y_end = coeffs(0) +
+                                    coeffs(1) * x_end +
+                                    coeffs(2) * x_end * x_end +
+                                    coeffs(3) * x_end * x_end * x_end;
+
+    const CppAD::AD<double> angle_end = CppAD::atan2(y_end, x_end);
+
+    /*
+      Adjust reference speed to slow down in sharp turns using simple
+      correlation to the lateral distance y_end of the planned path's
+      endpoint and convert from mph -> mps.
+    */
+    const CppAD::AD<double> ref_v_by_turn =
+                              (kRefVMax - kRefVTurnFactor * CppAD::abs(y_end))
+                              * (1609.34 / 3600);
   }
 };
 
