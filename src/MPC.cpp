a4b01abffca60336069a11d00d35d669dd451616
Diogo Pontes <dpontes11@gmail.com>
2018-01-15 16:12:36 +0100

    added FG_eval class

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -87,8 +87,109 @@ class FG_eval {
     const CppAD::AD<double> ref_v_by_turn =
                               (kRefVMax - kRefVTurnFactor * CppAD::abs(y_end))
                               * (1609.34 / 3600);
+
+    // Initialize cost to zero before adding up each cost term
+    fg[0] = 0;
+
+    /*
+      Add cost terms based on the reference state (cost term coefficients
+    were manually tuned to achieve ~110mph peak speed with smooth driving)
+    */
+    for(int t = 0; t < kNSteps; t++){
+      // Basic CTE
+      fg[0] += 20 * CppAD::pow(vars[epsi_start + t], 2) * t;
+
+      // Basic EPSI (progressive cost increases linearly by timesteps)
+      fg[0] += 32000 * CppAD::pow(vars[epsi_start + t], 2) * t;
+
+      // Difference from target reference velocity
+      fg[0] += 1500 * CppAD::pow(vars[v_start + t] - ref_v_by_turn, 2);
+
+      // Difference from angle to end point of horizon
+      // (progressive cost increases linearny by timestep)
+      fg[0] += 31000 * CppAD::pow(angle_end - vars[psi_start + t], 2) * t;
+    }
+
+    // Add cost terms to minimize the use of actuators
+    for(int t = 0; t < kNSteps; t++){
+      // Absolute steering angle
+      fg[0] += 200000 * CppAD::pow(vars[delta_start + t], 2);
+    }
+
+    // Add cost terms to minimize the change between sequential actuations
+    for(int t = 0; t < kNSteps - 2; t++){
+      // Change in sequential steering actuations
+      fg[0] += 100 * CppAD::pow(vars[delta_start + t + 1] -
+                                vars[delta_start + t], 2);
+
+      // Change in sequential throttle (acceleration) actuations
+      fg[0] += 5000 * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+    }
+
+    /*
+      Setup constraints
+      Constraints at initial timestep set to initial state values. Starting
+      indices of the 'fg' vector are incremented +1 to shift to after the Cost
+      value stored at index 0.
+    */
+    fg[1 + x_start]    = vars[x_start];
+    fg[1 + y_start]    = vars[y_start];
+    fg[1 + psi_start]  = vars[psi_start];
+    fg[1 + v_start]    = vars[v_start];
+    fg[1 + cte_start]  = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+
+    // Constraints at each time step set to follow motion model equations
+    for(int t = 1; t < kNSteps; t++){
+      // The state at time t1
+      const CppAD::<double> x1    = vars[x_start + t];
+      const CppAD::<double> y1    = vars[y_start + t];
+      const CppAD::<double> psi1  = vars[psi_start + t];
+      const CppAD::<double> v1    = vars[v_start + t];
+      const CppAD::<double> cte1  = vars[cte_start + t];
+      const CppAD::<double> epsi1 = vars[epsi_start + t];
+
+      // The state at previous time t0
+      const CppAD::<double> x0    = vars[x_start + t - 1];
+      const CppAD::<double> y0    = vars[y_start + t - 1];
+      const CppAD::<double> psi0  = vars[psi_start + t - 1];
+      const CppAD::<double> v0    = vars[v_start + t - 1];
+      const CppAD::<double> cte0  = vars[cte_start + t - 1];
+      const CppAD::<double> epsi0 = vars[epsi_start + t - 1];
+
+      // The actuation from time t0
+      const CppAD::AD<double> delta0 = vars[delta_start + t - 1];
+      const CppAD::AD<double> a0     = vars[a_start + t - 1];
+
+      // f(x) = coeffs[0] + coeffs[1]*x + coeffs[2]*x^2 + coeffs[3]*x^3
+      const CppAD::AD<double> f0 = coeffs(0) +
+                                    coeffs(1) * x0 +
+                                    coeffs(2) * (x0 * x0) +
+                                    coeffs(3) * (x0 * x0 * x0);
+
+      // psides = atan(f'(x))
+      //        = atan(coeffs[1] + 2*coeffs[2]*x + 3*coeffs[3]*x^2)
+      const CppAD::AD<double> psides0 = CppAD::atan(coeffs(1) + 2*coeffs(2)*x0
+                                        + coeffs(3)*x0*x0);
+
+      /*
+        Equations for the motion model:
+          x[t1]    = x[t0] + v[t0] * cos(psi[t0]) * dt
+          y[t1]    = y[t0] + v[t0] * sin(psi[t0]) * dt
+          psi[t1]  = psi[t0] + v[t0] / Lf * delta[t0] * dt;
+          v[t1]    = v[t0] + a[t0] * dt
+          cte[t1]  = (y[t0] - f(x[t0])) + (v[t0] * sin(epsi[t0]) * dt)
+          epsi[t1] = (psi[t0] - psides[t0]) + (v[t0] * delta[t0] / Lf *dt)
+       */
+       fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * kDeltaT);
+       fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * kDeltaT);
+       fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / kLf * kDeltaT);
+       fg[1 + v_start + t] = v1 - (v0 + a0 * kDeltaT);
+       fg[1 + cte_start + t] = cte1 - ((y0 - f0) + (v0 * CppAD::sin(epsi0) * kDeltaT));
+       fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + (v0 * delta0 / kLf * kDeltaT));
+    }
   }
-};
+}; // class FG_eval
 
 //
 // MPC class definition implementation.
--- src/MPC.h
+++ src/MPC.h
@@ -24,7 +24,7 @@ class MPC {
   std::vector<double> mpc_path_y_;  // MPC's planned path y coordinates
 
   // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuatotions.
+  // Return the first actuations.
   vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
